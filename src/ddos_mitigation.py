4c01ef2fe77cb62f70b0257b072c26c38550db73
TN3W <tn3w@protonmail.com>
2024-12-19 20:08:55 +0100

    Added not working utility functions

--- src/ddos_mitigation.py
+++ src/ddos_mitigation.py
@@ -6,14 +6,25 @@ by implementing a rate limiting mechanism based on IP addresses.
 """
 
 from time import time
-from typing import Final
+from random import choice, randint
+from typing import Final, Optional
+from datetime import datetime, timedelta
+from socket import gethostbyname, gaierror
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError, URLError
 
 try:
-    from src.utils import REDIS_CLIENT
+    from src.logger import log
     from src.crypto import sha256_hash_text
+    from src.internet_protocol import reverse_ip, is_ipv4
+    from src.utils import REDIS_CLIENT, cache_with_ttl, str_to_float, matches_rules
 except (ModuleNotFoundError, ImportError):
-    from utils import REDIS_CLIENT
+    from logger import log
     from crypto import sha256_hash_text
+    from internet_protocol import reverse_ip, is_ipv4
+    from utils import REDIS_CLIENT, cache_with_ttl, str_to_float, matches_rules
 
 
 DEFAULT_IP_HASH: Final[str] = "eCpiLALcButgO5xE90Xbt3Oa8Hd5WvScPomOSoP8bts"
@@ -49,3 +60,315 @@ def rate_limit(ip_address: str) -> bool:
     recent_requests = sum(1 for t in timestamps if current_time - int(t) <= 10)
 
     return recent_requests > 15
+
+
+MALICIOUS_ASNS: Final[str] = [
+    "Fastly", "Incapsula", "Akamai", "AkamaiGslb", "Google", "Datacamp Limited",
+    "Bing", "Censys", "Hetzner", "Linode", "Amazon", "AWS", "DigitalOcean", "Vultr",
+    "Azure", "Alibaba", "Netlify", "IBM", "Oracle", "Scaleway", "Cloud", "VPN"
+]
+
+
+def is_asn_malicious(asn: str) -> bool:
+    """
+    Determines if a given Autonomous System Number (ASN) is considered malicious.
+
+    Args:
+        asn (str): The Autonomous System Number to check.
+
+    Returns:
+        bool: True if the ASN is not malicious, False if it is malicious.
+    """
+
+    normalized_asn = asn.lower().strip()
+
+    for malicious_asn in MALICIOUS_ASNS:
+        if malicious_asn.lower() in normalized_asn:
+            return True
+
+    return False
+
+
+@cache_with_ttl(28800)
+def is_ip_malicious_ipapi(ip_address: str) -> Optional[bool]:
+    """
+    Uses the IPApi.com API to check the reputation of the given IP address.
+
+    Args:
+        ip_address (str): The IP address to check.
+        api_key (Optional[str]): The API key for authenticating with the IPApi.com API.
+                                 If provided, it will be included in the request.
+
+    Returns:
+        Optional[bool]: True if the IP address is malicious, False if it is not, or None
+                        if an error occurs.
+    """
+
+    url = f"http://ip-api.com/json/{ip_address}?fields=proxy,hosting"
+
+    data = http_request(url, is_json = True, default = {})
+    if not isinstance(data, dict):
+        return None
+
+    some_data_provided = False
+    for key in ["proxy", "hosting"]:
+        value = data.get(key, None)
+        if value is True:
+            return True
+
+        if value is not None:
+            some_data_provided = True
+
+    if not some_data_provided:
+        return None
+
+    return False
+
+
+@cache_with_ttl(28800)
+def is_ip_malicious_ipintel(ip_address: str, _: Optional[str] = None) -> Optional[bool]:
+    """
+    Uses the getipintel.net API to check the reputation of the given IP address.
+
+    Args:
+        ip_address (str): The IP address to check.
+
+    Returns:
+        Optional[bool]: True if the IP address is malicious, False otherwise.
+    """
+
+    random_email = ''.join(
+        choice(
+            "abcdefghijklmnopqrstuvwxyz"
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        ) for _ in range(randint(4, 9))
+    ) + choice(["@outlook.com", "gmail.com", "icloud.com", "aol.com"])
+
+    url = f"https://check.getipintel.net/check.php?ip={ip_address}&contact={random_email}"
+
+    data = http_request(url, default = "")
+    score = str_to_float(data)
+    if not score:
+        return None
+
+    if score > 0.90:
+        return True
+
+    return False
+
+
+@cache_with_ttl(28800)
+def is_ip_malicious_stopforumspam(ip_address: str) -> bool:
+    """
+    Uses the stopforumspam.org API to check the reputation of the given IP address.
+
+    Args:
+        ip_address (str): The IP address to check.
+
+    Returns:
+        Optional[bool]: True if the IP address is malicious, False otherwise.
+    """
+
+    url = f'https://api.stopforumspam.org/api?ip={ip_address}&json'
+
+    response_json = http_request(url, is_json = True, default = {})
+    if not isinstance(response_json, dict):
+        return False
+
+    if not response_json.get('success') == 1:
+        return False
+
+    ip_info = response_json.get("ip", {})
+    appears = ip_info.get("appears", 0)
+    frequency = ip_info.get("frequency", 0)
+
+    return appears >= 1 and frequency >= 2
+
+
+@cache_with_ttl(28800)
+def is_ip_malicious_geoip(ip_address: str, rules: Optional[tuple]) -> bool:
+    """
+    Checks the reputation of the given IP address using GeoIP databases.
+
+    Args:
+        ip_address (str): The IP address to check.
+
+    Returns:
+        bool: True if the IP address is found to be malicious.
+    """
+
+    geoip = get_geoip()
+
+    some_database_available = False
+    for db_name in ["city", "asn"]:
+        database: Optional[GeoIP] = geoip.get(db_name, None)
+        if database is None:
+            log(f"{db_name} is not available.", level = 3)
+            continue
+
+        some_database_available = True
+
+        ip_address_info = database.get(ip_address)
+        if db_name == "asn":
+            if is_asn_malicious(ip_address_info.get("asorg", "")):
+                return True
+
+        if not rules:
+            continue
+
+        if matches_rules(rules, ip_address_info):
+            return True
+
+    if not some_database_available:
+        return None
+
+    return False
+
+
+def is_ip_malicious(ip_address: str, third_parties: Optional[list] = None) -> bool:
+    """
+    Checks whether the given IP address is malicious.
+
+    Args:
+        ip_address (str): The IP address to check.
+        third_parties (Optional[list]): A list of third-party services to use for the check.
+
+    Returns:
+        bool: True if the IP address is malicious, False otherwise.
+    """
+
+    if not isinstance(ip_address, str):
+        return False
+
+    if third_parties is None:
+        third_parties = ["ipapi", "ipintel", "stopforumspam", "geoip"]
+
+    for third_party, third_party_function in {
+            "ipapi": is_ip_malicious_ipapi,
+            "ipintel": is_ip_malicious_ipintel,
+            "stopforumspam": is_ip_malicious_stopforumspam,
+        }.items():
+
+        is_allowed = False
+        for allowed_third_party in third_parties:
+            if allowed_third_party.lower().startswith(third_party):
+                is_allowed = True
+
+        if not is_allowed:
+            continue
+
+        api_key = None
+        if ":" in third_party:
+            found_api_key = third_party.split(":")[1].strip()
+            if len(found_api_key) > 1:
+                api_key = found_api_key
+
+        is_malicious = third_party_function(ip_address, api_key)
+        if is_malicious is True:
+            return True
+
+    if "geoip" in third_parties:
+        if is_ip_malicious_geoip(ip_address):
+            return True
+
+    return False
+
+
+@cache_with_ttl(28800)
+def is_ipv4_tor(ipv4_address: Optional[str] = None) -> bool:
+    """
+    Checks whether the given IPv4 address is Tor.
+
+    Args:
+        ipv4_address (str): The IPv4 address to check.
+
+    Returns:
+        bool: True if the IPv4 address is Tor, False otherwise.
+    """
+
+    query = reverse_ip(ipv4_address)
+
+    try:
+        resolved_ip = gethostbyname(query)
+
+        if resolved_ip == '127.0.0.2':
+            return True
+
+    except gaierror:
+        log(f"{ipv4_address} connection could not be established", level = 4)
+
+    return False
+
+
+@cache_with_ttl(28800)
+def is_ip_tor_exonerator(ip_address: Optional[str] = None) -> bool:
+    """
+    Checks whether the given IP address is Tor using the Exonerator service.
+
+    Args:
+        ip_address (str): The IPv6 address to check.
+
+    Returns:
+        bool: True if the IPv6 address is Tor, False otherwise.
+    """
+
+    today = (datetime.now() - timedelta(days = 2)).strftime('%Y-%m-%d')
+
+    base_url = "https://metrics.torproject.org/exonerator.html"
+    query_params = {
+        "ip": ip_address,
+        "timestamp": today,
+        "lang": "en"
+    }
+    url = f"{base_url}?{urlencode(query_params)}"
+
+    req = Request(
+        url, headers = {'Range': 'bytes=0-', "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+            " (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.3"
+        }
+    )
+    try:
+        with urlopen(req, timeout = 3) as response:
+            html = ''
+            while True:
+                chunk = response.read(128).decode('utf-8')
+                if not chunk:
+                    break
+
+                html += chunk
+                if "Result is positive" in html:
+                    return True
+
+    except (HTTPError, URLError, TimeoutError):
+        log(f"{ip_address} connection could not be looked up on Exonerator", level = 4)
+
+    return False
+
+
+def is_ip_tor(ip_address: str, third_parties: Optional[list] = None) -> bool:
+    """
+    Checks whether the given IP address is Tor.
+
+    Args:
+        ip_address (str): The IP address to check.
+        third_parties (Optional[list]): A list of third-party services to use for the check.
+    Returns:
+        bool: True if the IP address is Tor, False otherwise.
+    """
+
+    if not isinstance(ip_address, str):
+        return False
+
+    if third_parties is None:
+        third_parties = ["tor_hostname", "tor_exonerator"]
+
+    if "tor_hostname" in third_parties and is_ipv4(ip_address):
+        if is_ipv4_tor(ip_address):
+            return True
+
+    if "tor_exonerator" in third_parties:
+        if is_ip_tor_exonerator(ip_address):
+            return True
+
+    return False
--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,9 @@ including random string generation, file read/write operations, environment vari
 loading, and specialized file serialization classes.
 """
 
+from time import time
 from threading import Lock
+from functools import wraps
 from base64 import b64encode
 from io import TextIOWrapper
 from shutil import copy2, move
@@ -25,7 +27,7 @@ except ModuleNotFoundError:
     from logger import log
 
 
-REDIS_CLIENT: Final[StrictRedis] = StrictRedis(host='127.0.0.1', port=6379, decode_responses=True)
+REDIS_CLIENT: Final[StrictRedis] = StrictRedis(host='127.0.0.1', port=6379, decode_responses=False)
 
 CURRENT_DIRECTORY_PATH: Final[str] = path.dirname(path.abspath(__file__)) \
     .replace("\\", "/").replace("//", "/").replace("src", "").replace("//", "/")
@@ -49,6 +51,41 @@ for directory_path in [ASSETS_DIRECTORY_PATH, DATA_DIRECTORY_PATH]:
         continue
 
     makedirs(directory_path, exist_ok = True)
+
+
+def str_to_float(string: str) -> Optional[float]:
+    """
+    Convert a string representation of a number to a float.
+
+    Args:
+        string (str): The string representation of the number to convert.
+
+    Returns:
+        Optional[float]: The converted float value if the string is valid; 
+            otherwise, None.
+    """
+
+    if not string:
+        return None
+
+    if string.startswith('-'):
+        string = string[1:]
+        sign = -1
+    else:
+        sign = 1
+
+    parts = string.split('.')
+
+    if len(parts) > 2:
+        return None
+
+    if not all(part.isdigit() for part in parts):
+        return None
+
+    if len(parts) == 1:
+        return sign * float(parts[0])
+
+    return sign * float(parts[0] + '.' + parts[1])
 
 
 def generate_random_string(length: int, characters: str = "aA0!"):
@@ -97,6 +134,228 @@ def secure_shuffle(original_list: list) -> list:
             shuffled_list[random_index], shuffled_list[current_index]
 
     return shuffled_list
+
+
+
+def cache_with_ttl(ttl: int) -> callable:
+    """
+    Caches the result of a function with a given TTL.
+
+    Args:
+        ttl (int): The TTL in seconds.
+
+    Returns:
+        callable: The decorated function.
+    """
+
+    def decorator(func: callable) -> callable:
+        """
+        Internal decorator function.
+
+        Args:
+            func (callable): The function to decorate.
+
+        Returns:
+            callable: The decorated function.
+        """
+
+        cache = {}
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            """
+            Internal wrapper function.
+
+            Args:
+                *args: The positional arguments to pass to the function.
+                **kwargs: The keyword arguments to pass to the function.
+            """
+
+            key = (args, tuple(kwargs.items()))
+            current_time = time()
+
+            if key in cache:
+                result, timestamp = cache[key]
+                if current_time - timestamp < ttl:
+                    return result
+
+                del cache[key]
+
+            result = func(*args, **kwargs)
+            cache[key] = (result, current_time)
+
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+
+def matches_asterisk_rule(obj: str, asterisk_rule: str) -> bool:
+    """
+    Checks if a string matches a given asterisk rule.
+
+    Args:
+        obj (str): The string to check.
+        asterisk_rule (str): The asterisk rule to match against.
+
+    Returns:
+        bool: True if the string matches the rule, False otherwise.
+    """
+
+    if isinstance(obj, str) and isinstance(asterisk_rule, str) and '*' in asterisk_rule:
+        parts = asterisk_rule.split('*')
+
+        if len(parts) == 2:
+            start, end = parts
+            return obj.startswith(start) and obj.endswith(end)
+
+        first_asterisk_index = asterisk_rule.index('*')
+        last_asterisk_index = asterisk_rule.rindex('*')
+        start = asterisk_rule[:first_asterisk_index]
+        middle = asterisk_rule[first_asterisk_index + 1:last_asterisk_index]
+        end = asterisk_rule[last_asterisk_index + 1:]
+
+        return obj.startswith(start) and obj.endswith(end) and middle in obj
+
+    return obj == asterisk_rule
+
+
+def compare_numbers(field_data: Any, value: Any, morethan: bool = False) -> bool:
+    """
+    Compares two numbers based on the given operator.
+
+    Args:
+        field_data (Any): The first number (or string representation of a number).
+        value (Any): The second number to compare against.
+        morethan (bool): If True, checks if field_data is greater than value; 
+                         if False, checks if field_data is less than value.
+
+    Returns:
+        bool: True if the comparison is true, False otherwise.
+    """
+
+    if isinstance(field_data, str):
+        if field_data.is_digit():
+            field_data = int(field_data)
+
+    if not isinstance(field_data, int):
+        return False
+
+    if morethan:
+        return field_data > value
+
+    return field_data < value
+
+
+def check_string_start_end(field_data: Any, value: str, startswith: bool = False) -> bool:
+    """
+    Checks if a string starts or ends with a given value.
+
+    Args:
+        field_data (Any): The string to check (or integer to convert).
+        value (str): The value to check against.
+        startswith (bool): If True, checks if the string starts with the value; 
+                           if False, checks if it ends with the value.
+
+    Returns:
+        bool: True if the condition is met, False otherwise.
+    """
+
+    if isinstance(field_data, int):
+        field_data = str(field_data)
+    elif not isinstance(field_data, str):
+        return False
+
+    if startswith:
+        return field_data.startswith(value)
+
+    return field_data.endswith(value)
+
+
+def evaluate_operator(field_data: Any, operator: str, value: Any) -> bool:
+    """
+    Evaluates an operator against field data and a value.
+
+    Args:
+        field_data (Any): The data to evaluate.
+        operator (str): The operator to use for evaluation.
+        value (Any): The value to compare against.
+
+    Returns:
+        bool: True if the evaluation is true, False otherwise.
+    """
+
+    operator_actions = [
+        (
+            ['=', '==', 'equals', 'equal', 'is', 'sameas', 'thesameas', 'isthesameas'],
+            lambda: matches_asterisk_rule(field_data, value)
+        ),
+        (
+            ['!=', 'doesnotequal', 'doesnotequals', 'notequals', 'notequal', 'notis'],
+            lambda: not matches_asterisk_rule(field_data, value)
+        ),
+        (['contains', 'contain'], lambda: value in field_data),
+        (
+            ['doesnotcontain', 'doesnotcontains', 'notcontain', 'notcontains'],
+            lambda: value not in field_data
+        ),
+        (['@', 'isin', 'in'], lambda: field_data in value),
+        (['!@', 'isnotin', 'notisin', 'notin'], lambda: field_data not in value),
+        (['>', 'greaterthan', 'largerthan'], lambda: compare_numbers(field_data, value, True)),
+        (['<', 'lessthan'], lambda: compare_numbers(field_data, value)),
+        (
+            ['startswith', 'beginswith'],
+            lambda: check_string_start_end(field_data, value, True)
+        ),
+        (
+            ['endswith', 'concludeswith', 'finisheswith'],
+            lambda: check_string_start_end(field_data, value)
+        ),
+    ]
+
+    for operators, action in operator_actions:
+        if operator in operators:
+            return action()
+
+    return False
+
+
+def matches_rules(rules: tuple, fields: dict) -> bool:
+    """
+    Checks if a rule matches the given fields.
+
+    Args:
+        rules (tuple): The rules to check.
+        fields (dict): The fields to match against.
+
+    Returns:
+        bool: True if the rule matches the fields, False otherwise.
+    """
+
+    i = 0
+    for i, value in enumerate(rules):
+        if value == 'and':
+            return matches_rules(rules[:i], fields) and \
+                matches_rules(rules[i + 1:], fields)
+
+        if value == 'or':
+            return matches_rules(rules[:i], fields) or \
+                matches_rules(rules[i + 1:], fields)
+
+        i += 1
+
+    field, operator, value = rules
+    field_data = fields.get(field, None)
+
+    if field_data is None:
+        return False
+
+    if isinstance(operator, str):
+        operator = operator.strip(' ').lower()
+
+    return evaluate_operator(field_data, operator, value)
 
 
 def get_lock(file_path: str) -> Lock:
